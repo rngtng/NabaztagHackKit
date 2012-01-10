de0d8f2d33fcf5409b7aa796a3fd3016866efe62
Tobias Bielohlawek <tobi@soundcloud.com>
2012-01-10 23:23:09 +0100

    cleanup server

--- pinnab.rb
+++ pinnab.rb
@@ -1,8 +1,5 @@
 require 'sinatra'
 
-puts ENV["NR"]
-
-@@color = [0,0,0]
 @@reboot = ENV["NR"].nil?
 
 def full_message(data)
@@ -14,14 +11,8 @@ def message(cmd, *data)
   full_message [cmd] + [0x00, 0x00, data.size] + data
 end
 
-def ping(seconds = 30)
-  puts "ping"
-  message 3, seconds
-end
-
-def color
-  #puts "color #{@@color.join("-")}"
-  message 2, @@color
+def ok
+  message 3
 end
 
 def reboot
@@ -35,22 +26,13 @@ end
 
 ##################################
 
-get '/color' do
-  if params.any?
-    @@color = [params["r"].to_i, params["g"].to_i, params["b"].to_i]
-  end
-
-  color.map { |t| t.to_s(16) }.join("-")
-end
-
 get '/vl/bc.jsp' do
   puts "bootcode #{@@reboot}"
   @@reboot = false
   send_file File.join('public', 'bootcode', 'bootcode.bin')
 end
 
-get '/vl/rfid' do
-  puts params["tag"]
-  msg = (@@reboot || params["tag"] == "d0021a0352d64cec") ? reboot : color
+get '/vl/*' do
+  msg = @@reboot ? reboot : ok
   send_byte_array(msg)
 end
