18cf6fd7a79db40c09c571aeeb90afcdbab789fa
Tobias Bielohlawek <tobi@soundcloud.com>
2012-09-05 18:14:59 +0200

    updated server

--- examples/monitor/server.rb
+++ examples/monitor/server.rb
@@ -1,31 +1,35 @@
-
-require "nabaztag_hack_kit/server"
-
+require 'nabaztag_hack_kit/server'
 require 'nabaztag_hack_kit/message/helper'
 
+require './graphite_stats'
+
 class Server < NabaztagHackKit::Server
+  include GraphiteStats
 
   def initialize
     super #('bytecode.bin')
   end
 
   on "button-pressed" do |data, request|
-    send_nabaztag({
-      LED_L1   => [0],
-      LED_L2   => [0],
-      LED_L3   => [0],
-      LED_L4   => [0,0,0,0,100],
-    })
-  end
-
-  on "ping" do |data, request|
-    #send_nabaztag NabaztagHackKit::Message::Helper::fire
+    send_nabaztag OK
     # ({
     #   LED_L1   => [0],
-    #   LED_L2   => [0,100, 200, 300],
+    #   LED_L2   => [0],
     #   LED_L3   => [0],
     #   LED_L4   => [0,0,0,0,100],
     # })
+  end
+
+  on "ping" do |data, request|
+    send_nabaztag begin
+     if payment(GraphiteStats::KEY, params[:p])
+        NabaztagHackKit::Message::Helper::wink.merge(
+          NabaztagHackKit::Message::Helper::circle
+        )
+      else
+        NabaztagHackKit::Message::Helper::stop
+      end
+    end
   end
 end
 
