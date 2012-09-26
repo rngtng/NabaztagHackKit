51e736559e763e1b998194aecf98da7325dc8635
Tobias Bielohlawek <tobi@soundcloud.com>
2012-09-27 00:57:42 +0200

    added playground and bunny detector

--- lib/nabaztag_hack_kit/server.rb
+++ lib/nabaztag_hack_kit/server.rb
@@ -22,13 +22,45 @@ module NabaztagHackKit
       @bytecode_file = opts.fetch(:bytecode_file, public_file('bytecode.bin'))
     end
 
+    helpers do
+      def bunnies
+        @@bunnies ||= {}
+      end
+
+      def cmds
+        @@cmds ||= {}
+      end
+
+      def commands
+        Message::Api.constants.sort.inject({}) do |hash, constant|
+          if constant.to_s.length > 2
+            hash[constant] = Message::Api.const_get(constant)
+          end
+          hash
+        end
+      end
+
+      def distance_of_time(from_time, to_time = Time.now)
+        hours   = ( to_time - from_time) / 3600
+        minutes = ((to_time - from_time) % 3600) / 60
+        seconds = ((to_time - from_time) % 3600) % 60
+
+        "".tap do |diff_in_words|
+          diff_in_words << "%2dh,&nbsp;"   % hours   if hours.floor   > 0
+          diff_in_words << "%2dmin,&nbsp;" % minutes if minutes.floor > 0
+          diff_in_words << "%2dsec"        % seconds
+        end
+      end
+    end
+
     class << self
       def callbacks
         @@callbacks ||= {}
       end
 
       def on(callback, &block)
-        callbacks[callback] = block
+        callbacks[callback] ||= []
+        callbacks[callback] << block
       end
     end
 
@@ -49,9 +81,9 @@ module NabaztagHackKit
       end
     end
 
-    def callback(action, data, request)
-      if callback = self.class.callbacks[action.to_s]
-        instance_exec data, request, &callback
+    def callback(action, data, request, run = 0)
+      if (cb = self.class.callbacks[action.to_s]) && (callback = cb[run])
+        instance_exec data, request, run, &callback
       else
         logger.warn "no callback found for #{action}"
         send_nabaztag OK
@@ -59,7 +91,7 @@ module NabaztagHackKit
     end
 
     get "/" do
-      File.read public_file("index.html")
+      erb File.read(public_file("index.html.erb"))
     end
 
     get "/bc.jsp" do
@@ -71,29 +103,17 @@ module NabaztagHackKit
       end
     end
 
-    post "#{PREFIX}/log.jsp" do
-      parse_log(params[:logs]).tap do |logs|
-        logger.info "#########################"
-        logger.info logs.join("\n")
-        logger.info "#########################"
+    get "/favicon.ico" do
+    end
+
+    post "/commands" do
+      bunnies.each do |sn, value|
+        if params[:bunny] == "*" || params[:bunny] == sn
+          cmds[sn] ||= {}
+          cmds[sn][params[:command].to_i] = params[:values].split(",").map(&:to_i)
+        end
       end
-      send_nabaztag OK
-    end
-
-    post "#{PREFIX}/recording-finished.jsp" do
-      file_name = REC_FILE # TODO add timestamp??
-      File.open(file_name, "w+") do |f|
-        f.write request.body.read
-      end
-      callback('recording-finished', file_name, request)
-    end
-
-    get "#{PREFIX}/rfid.jsp" do
-      callback('rfid', params[:id], request)
-    end
-
-    get "#{PREFIX}/button-pressed.jsp" do
-      callback('button-pressed', params[:duration], request)
+      redirect "/"
     end
 
     get "/streams/:file.mp3" do
@@ -107,6 +127,7 @@ module NabaztagHackKit
     # generic callback
     %w(get post).each do |method|
       send(method, "#{PREFIX}/:action.jsp") do
+        bunnies[params[:bunnyid]] = Time.now
         callback(params[:action], params, request)
       end
     end
@@ -115,6 +136,36 @@ module NabaztagHackKit
     get "/*" do
       logger.warn "no route found for #{params[:splat]}"
       status 404
+    end
+
+    # default callback
+    on "log" do |data, request, run|
+      parse_log(data).tap do |logs|
+        logger.info "#########################"
+        logger.info logs.join("\n")
+        logger.info "#########################"
+      end
+      callback('log', data, request, run+1)
+    end
+
+    on "recording-finished" do |data, request, run|
+      file_name = REC_FILE # TODO add timestamp??
+      File.open(file_name, "w+") do |f|
+        f.write request.body.read
+      end
+      callback('recording-finished', file_name, request, run+1)
+    end
+
+    on "button-pressed" do |data, request, run|
+      callback('button-pressed', params[:duration], request, run+1)
+    end
+
+    on "ping" do |data, request, run|
+      if cmd = cmds.delete(params[:bunnyid])
+        send_nabaztag cmd
+      else
+        callback('ping', data, request, run+1)
+      end
     end
 
     protected
