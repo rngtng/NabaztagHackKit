35069c5dd9ee0e74db8de7d9a67c5a6a9e2ffda5
Tobias Bielohlawek <tobias.bielohlawek@vinted.com>
2026-06-26 21:31:25 +0200

    Add preprocessor
    preporc

---
+++ tools/preprocessor/scripts/preproc.pl
@@ -0,0 +1,144 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Time::Piece;
+
+$ENV{TZ} = 'Europe/Paris';
+my $datetime = localtime->datetime;
+
+my %defines;
+my %includedFiles;
+
+my @linesnums;
+my $fileLevel = 0;
+
+# process one file
+sub processThisFile
+{
+        my $ifdefLevel=0;
+        my $F = $_[0];
+        my $filename = $_[1];
+        my @ifdefskip;
+        my $linenum;
+        my $skippedLastLine=0;
+        my $isHtml = $filename =~ /\.html|\.css$/i;
+
+        $fileLevel++;
+        $ifdefskip[$ifdefLevel] = 0;
+
+        while (my $line = <$F>) {
+                $line =~ s/(__DATE__)/$datetime/g;
+                if ($isHtml) {
+                    # quote "
+                    $line =~ s/(["])/\\$1/g;
+                }
+                $linenum++;
+                if ($line =~ m/^#ifdef ([a-zA-Z0-9_-]+)/) {
+                        if ($ifdefLevel > 0 && $ifdefskip[$ifdefLevel]) {
+                            # nested ifdef, inherit skip status
+                            $ifdefskip[$ifdefLevel+1] = $ifdefskip[$ifdefLevel];
+                            $ifdefLevel++;
+                        } else {
+                            $ifdefLevel++;
+                            $ifdefskip[$ifdefLevel] = (! exists $defines{$1});
+                        }
+                }
+                elsif ($line =~ m/^#ifndef ([a-zA-Z0-9_-]+)/) {
+                        $ifdefLevel++;
+                        $ifdefskip[$ifdefLevel] = (exists $defines{$1});
+                }
+                elsif ($line =~ m/^#else/) {
+                        if ($ifdefLevel > 0) {
+                                $ifdefskip[$ifdefLevel] = !($ifdefskip[$ifdefLevel]);
+                        }
+                        else {
+                                die "#else without matching #ifdef";
+                        }
+                }
+                elsif ($line =~ m/^#endif/) {
+                        if ($ifdefLevel > 0) {
+                                $ifdefLevel--;
+                        }
+                        else {
+                                die "#endif without matching #ifdef";
+                        }
+                }
+                else
+                {
+                        if (0 != $ifdefskip[$ifdefLevel]) {
+                                $skippedLastLine=1;
+                                next ; # skip the line
+                        }
+                        if (1==$skippedLastLine) {
+                                print "// file: $filename, line $linenum\n";
+                        }
+                        $skippedLastLine=0;
+                        if ($line =~ m/^#define ([a-zA-Z0-9_-]+)/) {
+                                $defines{$1} = 1;
+                        }
+                        elsif ($line =~ m/^#include ([a-zA-Z0-9.\/_-]+)/) {
+                                my $includedFile=$1;
+                                if (exists $includedFiles{$includedFile})
+                                {
+                                        print "// file $includedFile already included\n";
+                                        next ;
+                                }
+                                if (! $includedFile =~ /\.html|\.css$/i) {
+                                    print "// file $includedFile //\n";
+                                }
+                                my $includedFileHandle;
+                                open($includedFileHandle, $includedFile) || die "Could not open file $includedFile" ;
+                                $includedFiles{$includedFile} = 1;
+                                my $ret = processThisFile($includedFileHandle, $includedFile);
+                                close($includedFileHandle);
+                                if (0 != $ret) {
+                                        print "Could not include file $includedFile";
+                                        exit 1 ;
+                                }
+                                if (! $includedFile =~ /\.html|\.css$/i) {
+                                    print "// end of file $includedFile //\n";
+                                    print "// back to file $filename, line $linenum\n";
+                                }
+                        }
+                        else {
+                                print "$line";
+                        }
+                }
+        }
+
+        $fileLevel--;
+
+        return ($ifdefLevel != 0) ;
+}
+
+
+# process main file
+my $mainFileHandle;
+my $filename;
+my $state;
+my $arg;
+
+$filename = "STDIN";
+$mainFileHandle = *STDIN ;
+
+$state = 0;
+foreach $arg (@ARGV) {
+    if($arg eq "-D") {
+        $state = 1;
+    } else {
+        if($state == 1) {
+            $state = 0;
+            $defines{$arg} = 1;
+        } else {
+            $filename = $arg;
+        }
+    }
+}
+unless ($filename eq "STDIN") {
+        open($mainFileHandle, $filename);
+        $includedFiles{$filename} = 1;
+}
+
+processThisFile($mainFileHandle, $filename);
+
+close($mainFileHandle);
