58f3f74f32980217312efad2cfca8076213318b3
Tobias Bielohlawek <tobias.bielohlawek@vinted.com>
2026-06-26 22:36:32 +0200

    Fix preproc
    sd
    
    prepoc

--- tools/preprocessor/scripts/make_nominal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CURRENTDIR=$(dirname $0)
-cd $CURRENTDIR/../firmware/
-
-PREPROC="../scripts/preproc.pl"
-PREPROC2="../scripts/preproc_remove_extra_protos.py"
-
-"$PREPROC" $* < main.mtl | "$PREPROC2" > "../nominal.mtl"
